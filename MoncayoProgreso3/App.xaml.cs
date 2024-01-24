e6fe56611f63051011a8e7843fb30bd4262af24c
Jorge Isaac Moncayo Loaiza <jorge.moncayo.loaiza@udla.edu.ec>
2024-01-24 07:12:53 -0500

    Agregar archivos de proyecto.

---
+++ MoncayoProgreso3/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MoncayoProgreso3;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MoncayoProgreso3/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MoncayoProgreso3;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
